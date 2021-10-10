d894d9d930e950421d4b40efd35ec282cb546373
Dorian Marié <dorian@dorianmarie.fr>
2021-10-10 17:16:03 +0200

    fix and handle multiple files

--- lib/dorian/yaml/read-write.rb
+++ lib/dorian/yaml/read-write.rb
@@ -2,14 +2,16 @@ require 'yaml'
 
 module Dorian
   module Yaml
-    class Compare
+    class ReadWrite
       def self.run
-        if ARGV.size != 1
-          puts 'USAGE: yaml-read-write FILE'
+        if ARGV.size < 1
+          puts 'USAGE: yaml-read-write FILE [FILE...]'
           exit
         end
 
-        File.write(ARGV.first, YAML.safe_load(File.read(ARGV.first)).to_yaml)
+        ARGV.each do |filename|
+          File.write(filename, YAML.safe_load(File.read(filename)).to_yaml)
+        end
       end
     end
   end
