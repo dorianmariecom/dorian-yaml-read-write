7cf4e3bc63a72d95447967286096e9187df05662
Dorian Marié <dorian@dorianmarie.fr>
2024-01-31 11:42:54 +0100

    lint and format

--- dorian-yaml-read-write.gemspec
+++ dorian-yaml-read-write.gemspec
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 Gem::Specification.new do |s|
   s.name = "dorian-yaml-read-write"
   s.version = "0.2.0"
   s.summary = "Reads and writes a YAML file"
-  s.description =
-    s.summary + "\n\n" + "e.g. `yaml-read-write config/locales/en.yml`"
+  s.description = "#{s.summary}\n\ne.g. `yaml-read-write config/locales/en.yml`"
   s.authors = ["Dorian Marié"]
   s.email = "dorian@dorianmarie.fr"
   s.files = ["lib/dorian/yaml/read-write.rb"]
--- lib/dorian/yaml/read-write.rb
+++ lib/dorian/yaml/read-write.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require "yaml"
 
 module Dorian
   module Yaml
     class ReadWrite
       def self.run
-        if ARGV.size < 1
+        if ARGV.empty?
           puts "USAGE: yaml-read-write FILE [FILE...]"
           exit
         end
 
         ARGV.each do |filename|
-          File.write(filename, YAML.safe_load(File.read(filename)).to_yaml)
+          File.write(filename, YAML.safe_load_file(filename).to_yaml)
         end
       end
     end
